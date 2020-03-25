e59e7c1272df08912a3f8cc0cfd757417390d7d9
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-03-25 14:22:53 +0100

    Implement Layout and add onClick method to btnOk button

--- DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
+++ DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.datastoragemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btnOk){
+            Log.i("btnOk", "btnOk clicked");
+        }
+
     }
 }
