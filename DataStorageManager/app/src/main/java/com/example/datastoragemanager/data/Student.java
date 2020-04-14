cd3270215256b11fbfdc423fa46a1e93355da8d5
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-04-14 10:33:03 +0200

    Create Student Entity and DAO

--- DataStorageManager/app/src/main/java/com/example/datastoragemanager/data/Student.java
+++ DataStorageManager/app/src/main/java/com/example/datastoragemanager/data/Student.java
@@ -1,4 +1,35 @@
 package com.example.datastoragemanager.data;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "student")
 public class Student {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private int id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String country;
+
+    public Student(int id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
--- DataStorageManager/app/src/main/java/com/example/datastoragemanager/data/StudentDao.java
+++ DataStorageManager/app/src/main/java/com/example/datastoragemanager/data/StudentDao.java
@@ -1,4 +1,17 @@
 package com.example.datastoragemanager.data;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface StudentDao {
+
+    @Insert
+    void InsertStudent(Student student);
+
+    @Query("SELECT * from student")
+    List<Student> selectAll();
 }
